832b463fbee3a7e3aa215f72f3ff99334788193b
Kapil Dadhich <kapildadhich075@gmail.com>
2024-12-27 01:24:54 +0530

    update: final

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -6,11 +6,12 @@ function Header() {
       <a
         href="https://drive.google.com/file/d/1SpaDQ2kX4irXEQS9SMLuf20FDl3LZYVq/view?usp=sharing"
         target="_blank"
-        className="bg-black px-4 sm:px-6 lg:px-8 py-2 sm:py-3 rounded-full text-white outline outline-white font-[anzo2] hover:bg-black/50"
+        className="bg-black px-4 sm:px-6 lg:px-8 py-2 sm:py-3 
+        rounded-full text-white outline outline-white font-[anzo2] hover:bg-black/50"
       >
         Resume!
       </a>
-      <i className="ri-menu-3-fill text-2xl sm:text-3xl ml-2 sm:ml-3 lg:ml-5 text-white"></i>
+      {/* <i className="ri-menu-3-fill text-2xl sm:text-3xl ml-2 sm:ml-3 lg:ml-5 text-white"></i> */}
     </div>
   );
 }
