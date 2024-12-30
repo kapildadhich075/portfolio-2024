c49dd13d3bdcc26a91d710cfa51a531f3198eeaa
Kapil Dadhich <kapildadhich075@gmail.com>
2024-12-31 00:33:04 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className="fixed w-full z-10 p-4 sm:p-8 lg:p-24 flex items-center justify-end py-4 sm:py-8 lg:py-28 px-4 sm:px-10 lg:px-20">
       <a
-        href="https://drive.google.com/file/d/1SpaDQ2kX4irXEQS9SMLuf20FDl3LZYVq/view?usp=sharing"
+        href="https://drive.google.com/file/d/1TyzFHQEyGljl8WoqoOez2Jt5K3DmAwrb/view?usp=sharing"
         target="_blank"
         className="bg-black px-4 sm:px-6 lg:px-8 py-2 sm:py-3 
         rounded-full text-white outline outline-white font-[anzo2] hover:bg-black/50"
