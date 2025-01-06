7999fbf8f7e1c77cf6bf0aaf75a169e4903f324d
Kapil Dadhich <39085540+kapildadhich075@users.noreply.github.com>
2025-01-06 16:49:50 +0530

    Update data.jsx

--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -81,7 +81,7 @@ export const DataofExperience = [
     place: "Remote",
     imgSrc: "",
     description:
-      "Developed advanced trip planning features that boosted user satisfaction by 25% and integrated seamless payment solutions like PhonePe, improving transaction success rates by 10%. Built a React Native food delivery app with Expo and Google APIs, enabling real-time rider tracking and enhancing delivery accuracy by 20%. Delivered scalable frontend solutions for job boards and delivery platforms using Next.js, PostgreSQL, Chakra UI, and React Native. Spearheaded the development of Trekykart's website and admin panel with React.js, Next.js, and Node.js, reducing page load times by 20% and increasing user engagement by 15%.",
+      "Developed scalable web apps using Next.js, TypeScript, and Tailwind CSS, delivering responsive solutions for clients like Fizzwizz Media. Integrated advanced AI with Eden AI for NLP and Twilio for automated voice responses. Built a CMS-driven platform using React, PostgreSQL, and Contentful CMS. Optimized performance with ImageKit, ensuring faster load times and superior user experiences.",
   },
   {
     designation: "Software Developer",
